b2f6955c7092392e2cb8c485b7d5d61b23e6b32f
David Morales <dmoralessal87@gmail.com>
2021-05-30 17:47:19 +0200

    Buscar un elemento en concreto y agregarlo al carrito

--- Cypress/cypress/integration/examples/PrimerasPruebas.spec.js
+++ Cypress/cypress/integration/examples/PrimerasPruebas.spec.js
@@ -16,6 +16,29 @@ describe('Primer conjunto de casos de prueba', () => {
     });
 
     // Caso 2
+    it('Agregar el elemento de tipo "blouse" al carrito desde la home', () => {
+
+        cy.visit('http://automationpractice.com/index.php');
+
+        // Obtener los elementos #homefeatured .product-container como un parámetro
+        cy.get('#homefeatured .product-container').as('PopularProducts');
+
+        // Iteramos en cada elemento de la lista para encontrar el producto
+        cy.get('@PopularProducts')
+        .find('.product-name')
+        .each(($el, index, $list) => {
+            // Cada elemento tiene un atributo title que contiene el nombre del producto
+            if($el.attr('title') === 'Blouse') {
+                
+                // Una vez lo encontramos mostramos un mensaje por consola
+                cy.log('Se ha encontrado el elemento buscado');
+
+                // Comprobamos que en el elemento en el que estamos tenga el botón 'Add to cart'
+                // si lo tiene clicamos sobre el
+                cy.get('@PopularProducts').eq(index).contains('Add to cart').click();
+            }
+        });
+    });
 
     // Caso 3
 });
