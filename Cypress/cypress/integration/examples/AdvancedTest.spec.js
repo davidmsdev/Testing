dba42efbc6669631b8d25ebf4badc304d4c97c84
David Morales <dmoralessal87@gmail.com>
2021-10-04 13:08:40 +0200

    Refactorizado código usando el método propio y recorriendo el array de productos

--- Cypress/cypress/integration/examples/AdvancedTest.spec.js
+++ Cypress/cypress/integration/examples/AdvancedTest.spec.js
@@ -18,57 +18,15 @@ describe('Pruebas más avanzadas', () => {
     it('Realizar compra de móviles basadas en su título', function() {
 
         // Móviles
-        let htc = this.data.mobiles[0]
-        let iPhone = this.data.mobiles[1]
-        let palmTreo = this.data.mobiles[2]
-        
-
-        cy.log(htc)
-
+        let products = this.data.mobiles
+    
         cy.get('#menu ul a:contains(Phones & PDAs)')
             .click()
 
-        // Obtenemos todos los elementos de la parrilla
-        cy.get('div[class="product-thumb"]').as('productsContainer')
-        cy.get('@productsContainer')
-            .each( ($el, index) => {
-
-                // Para cada elemento buscamos el nombre que queremos
-                cy.get(':has(.caption) h4 a').eq(index).then( $product => {
-                    let product = $product.text()
-                    
-                    if (product.includes(htc)) {
-                        cy.log('Se ha encontrado el elemento HTC Touch HD')
-
-                        // Buscamos el botón para añadir del elemento en el que estamos
-                        cy.get('@productsContainer').eq(index).find('button[onclick^="cart.add"]')
-                            .click()
-                    }
-                })
-
-                cy.get(':has(.caption) h4 a').eq(index).then( $product => {
-                    let product = $product.text()
-                    
-                    if (product.includes(iPhone)) {
-                        cy.log('Se ha encontrado el elemento HTC Touch HD')
-
-                        // Buscamos el botón para añadir del elemento en el que estamos
-                        cy.get('@productsContainer').eq(index).find('button[onclick^="cart.add"]')
-                            .click()
-                    }
-                })
-
-                cy.get(':has(.caption) h4 a').eq(index).then( $product => {
-                    let product = $product.text()
-                    
-                    if (product.includes(palmTreo)) {
-                        cy.log('Se ha encontrado el elemento HTC Touch HD')
-
-                        // Buscamos el botón para añadir del elemento en el que estamos
-                        cy.get('@productsContainer').eq(index).find('button[onclick^="cart.add"]')
-                            .click()
-                    }
-                })
-            })
+        // Obtenemos todos los elementos de la parrilla usando nuestro comando personalizado
+        products.forEach( (product) => {
+            cy.log(product)
+            cy.addProduct(product)
+        })      
     })
 })
