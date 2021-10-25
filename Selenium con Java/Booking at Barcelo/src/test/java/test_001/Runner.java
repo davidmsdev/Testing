0dd997ca377ff4c1568133bcd0086b3f4eb42dae
David Morales <david.morales@alten.es>
2021-10-25 10:39:18 +0200

    Maven build

--- Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
+++ Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
@@ -1,4 +1,4 @@
-package alten_001;
+package test_001;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,5 +19,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Runner {
 
+    @Parameters({"myName"})
+    @Test
+    public void parameterTest(String myName) {
+        System.out.println("Valor del parametro: " + myName);
+    }
 	
 }
