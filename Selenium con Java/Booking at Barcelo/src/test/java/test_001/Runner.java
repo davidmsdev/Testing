8406b54e6aae05b49ee115d87fc0af4f41e65092
David Morales <david.morales@alten.es>
2021-10-25 13:18:54 +0200

    Navigate to barcelo

--- Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
+++ Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
@@ -1,28 +1,66 @@
 package test_001;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+
+import pages.*;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class Runner {
 
-    @Parameters({"myName"})
-    @Test
-    public void parameterTest(String myName) {
-        System.out.println("Valor del parametro: " + myName);
+    // Instances
+    Utilities utilities = new Utilities();
+    HomePage homePage = new HomePage();
+
+    // Variables
+    WebDriver driver = null;
+	WebDriverWait wait = null;
+	static ExtentReports extent = new ExtentReports();
+	static ExtentSparkReporter spark = new ExtentSparkReporter("index.html");
+
+    String url = "";
+    String author = "David Morales";
+    ExtentTest testBuild = null;
+
+    @BeforeSuite
+    static void setupClass() {
+        
+    }
+
+    @DataProvider (name = "data-provider")
+    public Object[][] dpMethod(){
+
+        String name = "Sants";
+        // We obtain the date of the current day at 10:00:00 in milliseconds
+        LocalDate currentDate = LocalDate.now();
+        long currentDateInEpoch = currentDate.toEpochSecond(LocalTime.of(10, 00, 00), ZoneOffset.UTC);
+
+        System.out.println(currentDateInEpoch);
+        return new Object[][] {
+            { "First-Value", 2 , "tercero" }, 
+        };
+    }
+    
+    @Test (dataProvider = "data-provider")
+    public void parameterTest(String val1, int val2, String val3) throws InterruptedException {
+
+        // Load properties
+        utilities.getProperties();
+
+        url = utilities.getURL();
+
+        homePage.navigateToBarceloPage(url);
+
     }
 	
 }
