7302c65caa99a302e5a4a175ecd301283c392745
David Morales <david.morales@alten.es>
2021-10-25 17:37:47 +0200

    Enter the hotel name and select in dropdown menu

--- Selenium con Java/Booking at Barcelo/src/main/java/pages/HomePage.java
+++ Selenium con Java/Booking at Barcelo/src/main/java/pages/HomePage.java
@@ -2,12 +2,37 @@ package pages;
 
 public class HomePage extends BasePage {
 
+    // Locators
+    String cookiesButton = "//*[@id='main-cookies-dialog']/div/div/div/div[2]/button";
+    String hotelNameInputLocator = "//input[@id='destination-fb']";
+    String hotelNameDropDownResults = "//div[@class='c-fastbooking__submenu-search-hotels']/ul/li/strong[contains(text(), 'Sants')]";
+
+    // Variables
+    String hotelName = "";
+
     public HomePage() {
         super(driver);
     }
 
     public void navigateToBarceloPage(String url) {
         navigateTo(url);
+    }
+
+    public void clickCookies() {
+        clickElement(cookiesButton);
+    }
+
+    public void enterHotelName(String name) {
+        hotelName = name;
+        write(hotelNameInputLocator, hotelName);
+    }
+
+    public String getInputHotelNameValue() {
+        return find(hotelNameInputLocator).getAttribute("value");
+    }
+
+    public void clickHotelNameInDropDownResults() {
+        clickElement(hotelNameDropDownResults);
     }
     
 }
