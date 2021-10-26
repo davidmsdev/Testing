adb44f35234108573d506fdae6e8f238a5caf753
David Morales <david.morales@alten.es>
2021-10-26 17:08:17 +0200

    Sending the guest

--- Selenium con Java/Booking at Barcelo/src/main/java/pages/HomePage.java
+++ Selenium con Java/Booking at Barcelo/src/main/java/pages/HomePage.java
@@ -1,8 +1,6 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage {
 
@@ -10,11 +8,16 @@ public class HomePage extends BasePage {
     String cookiesButton = "//*[@id='main-cookies-dialog']/div/div/div/div[2]/button";
     String hotelNameInputLocator = "//input[@id='destination-fb']";
     String hotelNameDropDownResults = "//div[@class='c-fastbooking__submenu-search-hotels']/ul/li/strong[contains(text(), 'Sants')]";
+    String selectGuestButton = "rooms-fb";
+    String adultsInputText = "//input[contains(@name,'adults')]";
+    String cildrensInputText = "//input[@name='childs']";
+    String bookingButtonXpath = "//button[@id='fastbooking_cta_booking_home']";
 
     // Variables
     String hotelName = "";
     String checkinXpath = "";
     String checkoutXpath = "";
+    WebElement bookingButton = null;
 
     public HomePage() {
         super(driver);
@@ -49,5 +52,21 @@ public class HomePage extends BasePage {
     public void clickInCheckoutDate(long checkout) {
         checkoutXpath = "//*[@id='month-2-1']/tbody/tr[1]/td[@time=" + checkout +"]";
         clickElement(checkoutXpath);
+    }
+
+    public void clickToSelectGuest() {
+        findById(selectGuestButton).click();
+    }
+
+    public void enterTheNumberOfAdults(String adults) {
+        write(adultsInputText, adults);
+    }
+
+    public void enterTheNumberOfChildrens(String childrens) {
+        write(cildrensInputText, childrens);
+    }
+
+    public void clickBookingButton() {
+        clickElement(bookingButtonXpath);
     }
 }
