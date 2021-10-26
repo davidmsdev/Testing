fcb201cacac8e1b375833452505e78c8541a6f7e
David Morales <david.morales@alten.es>
2021-10-26 13:47:54 +0200

    Add checkin and checkout

--- Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
+++ Selenium con Java/Booking at Barcelo/src/test/java/test_001/Runner.java
@@ -2,7 +2,9 @@ package test_001;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.NoSuchElementException;
 
 import pages.*;
@@ -13,9 +15,9 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -51,19 +53,28 @@ public class Runner {
 
         String browser = "Chrome";
         String name = "Barceló Sants";
+        // long oneDayInSeconds = 86400;
+        // long oneDayInMiliseconds = oneDayInSeconds * 1000;
+        // long add5days = oneDayInMiliseconds * 5;
         // We obtain the date of the current day at 10:00:00 in milliseconds
-        LocalDate currentDate = LocalDate.now();
-        long currentDateInEpoch = currentDate.toEpochSecond(LocalTime.of(10, 00, 00), ZoneOffset.UTC);
+        ZonedDateTime currentDate = ZonedDateTime.of(LocalDate.now(), LocalTime.of(12, 00, 00), ZoneId.of("Europe/Madrid"));
+        long checkin = (currentDate.plus(5, ChronoUnit.DAYS).toEpochSecond())*1000;
+        long checkout = (currentDate.plus(9, ChronoUnit.DAYS).toEpochSecond())*1000;
 
         return new Object[][] {
-            {browser, name}, 
+            {browser, name, checkin, checkout}, 
         };
     }
     
     @Test (testName = "Booking Barceló", dataProvider = "data-provider")
-    public void parameterTest(String browserName, String hotelName) throws InterruptedException {
+    public void parameterTest(String browserName, String hotelName, long checkin, long checkout) throws InterruptedException {
 
         ExtentTest test = extent.createTest("Booking in Barceló");
+
+        System.out.println(browserName);
+        System.out.println(hotelName);
+        System.out.println(checkin);
+        //System.out.println(checkout);
 
         // If it is not the correct browser, the test will stop
         try {
@@ -92,12 +103,21 @@ public class Runner {
         homePage.enterHotelName(hotelName);
         homePage.clickHotelNameInDropDownResults();
 
+        // The name of the input has to be the same as the name we write
         try {
             Assert.assertEquals(homePage.getInputHotelNameValue(), hotelName);
             test.pass("The name of the hotel was entered, it was selected in the dropdown and it appears correctly in the input");
         } catch (AssertionError e) {
             test.fail("The hotel name isn't correct");
         }
+
+        try {
+            homePage.clickInCheckinDate(checkin);
+        } catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            homePage.clickInCheckinDate(checkin);
+        }
+        
+        homePage.clickInCheckoutDate(checkout);
         
     }
 }
