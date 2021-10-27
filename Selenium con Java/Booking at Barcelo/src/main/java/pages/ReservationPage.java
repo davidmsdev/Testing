bb030b2f7c8e75cfc1accf7d2ca663df7c62c664
David Morales <david.morales@alten.es>
2021-10-27 13:56:58 +0200

    Added get the text of guest

--- Selenium con Java/Booking at Barcelo/src/main/java/pages/ReservationPage.java
+++ Selenium con Java/Booking at Barcelo/src/main/java/pages/ReservationPage.java
@@ -1,8 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+
 public class ReservationPage extends BasePage {
 
     private static final String CLOSEPOPUPPATH = "//a[@href='close-modal']";
+    private static final String ADULTSPATH = "//div[@class='reservation-cart-container_guests']/child::span[1]";
+    private static final String CHILDRENSPATH = "//div[@class='reservation-cart-container_guests']/child::span[2]";
 
     public ReservationPage() {
         super(driver);
@@ -10,5 +14,13 @@ public class ReservationPage extends BasePage {
 
     public void closePopup() {
         clickElement(CLOSEPOPUPPATH);
+    }
+
+    public String getAdultsGuestText() {
+        return getText(ADULTSPATH);
+    }
+
+    public String getChildrensGuestText() {
+        return getText(CHILDRENSPATH);
     }
 }
