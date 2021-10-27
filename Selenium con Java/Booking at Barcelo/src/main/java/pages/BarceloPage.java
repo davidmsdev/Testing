1afd6fe24bd144084c879fd214cd92a52e2bc679
David Morales <david.morales@alten.es>
2021-10-27 10:50:51 +0200

    Change variable to static final

--- Selenium con Java/Booking at Barcelo/src/main/java/pages/BarceloPage.java
+++ Selenium con Java/Booking at Barcelo/src/main/java/pages/BarceloPage.java
@@ -2,14 +2,14 @@ package pages;
 
 public class BarceloPage extends BasePage {
 
-    String bookingButton = "//button[@id='fastbooking_cta_booking_hotel']";
+    private static final String BOOKINGBUTTON = "//button[@id='fastbooking_cta_booking_hotel']";
 
     public BarceloPage() {
         super(driver);
     }
 
     public void clickBookingButton() {
-        clickElement(bookingButton);
+        clickElement(BOOKINGBUTTON);
     }
 
     public void changeWindow() {
