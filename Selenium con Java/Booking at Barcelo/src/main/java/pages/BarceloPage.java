bf37bd176cbf3f5f53406eedec9de89c33644e9e
David Morales <david.morales@alten.es>
2021-10-27 13:57:23 +0200

    Added path title

--- Selenium con Java/Booking at Barcelo/src/main/java/pages/BarceloPage.java
+++ Selenium con Java/Booking at Barcelo/src/main/java/pages/BarceloPage.java
@@ -3,6 +3,7 @@ package pages;
 public class BarceloPage extends BasePage {
 
     private static final String BOOKINGBUTTON = "//button[@id='fastbooking_cta_booking_hotel']";
+    private static final String TITLE = "//title";
 
     public BarceloPage() {
         super(driver);
