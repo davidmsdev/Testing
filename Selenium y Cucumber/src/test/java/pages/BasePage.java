7c47c4f824aed47595a4d9d12f7e582bec266309
David Morales <david.morales@alten.es>
2021-11-02 12:51:54 +0100

    Creada Base page

---
+++ Selenium y Cucumber/src/test/java/pages/BasePage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class BasePage {
+    
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+
+    // Static initializer block
+    static {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    // Constructor
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    // Methods
+    public static void navigateTo(String url) {
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    private WebElement Find(String locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+
+    public void clickElement(String locator) {
+        Find(locator).click();
+    }
+
+
+}
