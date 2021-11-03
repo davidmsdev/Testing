0c372d71b197f06b05d1bc7c5378ce775b67769e
David Morales <david.morales@alten.es>
2021-11-03 13:04:55 +0100

    Runner eliminado publish

--- Selenium y Cucumber/src/test/java/runner/Runner.java
+++ Selenium y Cucumber/src/test/java/runner/Runner.java
@@ -2,15 +2,14 @@ package runner;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "steps",
-    publish = true,
     tags = "@Test"
 )
 
