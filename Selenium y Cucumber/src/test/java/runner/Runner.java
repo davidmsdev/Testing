8c079b53dfb9de2565a703c0bfde52b1f92326ef
David Morales <david.morales@alten.es>
2021-11-09 10:58:57 +0100

    Cambios

--- Selenium y Cucumber/src/test/java/runner/Runner.java
+++ Selenium y Cucumber/src/test/java/runner/Runner.java
@@ -12,6 +12,8 @@ import pages.BasePage;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "steps",
+    plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports.json"},
+    monochrome = true,
     tags = "@Test"
 )
 
